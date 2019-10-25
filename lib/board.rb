9d4ecb0f6e8ca74746923b681b77ed4852e4de59
Taiye Salami <taiye41@gmail.com>
2019-10-25 19:58:27 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+
+board = []
+board[0] = " "
+board[0] = " "
+board[0] = " "
+board[0] = " "
+board[0] = " "
+board[0] = " "
+board[0] = " "
+board[0] = " "
+board[0] = " "
